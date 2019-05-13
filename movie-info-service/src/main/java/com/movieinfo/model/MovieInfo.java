6347d0861da48f0d9ee7a4798d24ce2858fdf96b
ankit <soodankit1993@gmail.com>
2019-05-14 00:37:20 +0530

    Adding controller for movie info service with configured port.

---
+++ movie-info-service/src/main/java/com/movieinfo/model/MovieInfo.java
@@ -0,0 +1,26 @@
+package com.movieinfo.model;
+
+public class MovieInfo {
+	private String movieId;
+	private String name;
+	
+	public MovieInfo(String movieId, String name) {
+		super();
+		this.movieId = movieId;
+		this.name = name;
+	}
+	public String getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ movie-info-service/src/main/java/com/movieinfo/services/MovieInfoServices.java
@@ -0,0 +1,5 @@
+package com.movieinfo.services;
+
+public class MovieInfoServices {
+
+}
