479ef7308df8ad583dde0a16b76ed273066866b1
ankit <soodankit1993@gmail.com>
2019-06-01 01:45:33 +0530

    Adding support for web client calls.

--- movie-catalog-services/src/main/java/com/moviecatalog/service/MovieCatalogService.java
+++ movie-catalog-services/src/main/java/com/moviecatalog/service/MovieCatalogService.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.moviecatalog.model.CatalogItem;
 import com.moviecatalog.model.MovieInfo;
@@ -17,13 +18,23 @@ public class MovieCatalogService {
 	
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
 
 	public List<CatalogItem> getCatalog(String userId){
 		//get all the rated movies
 		List<Ratings> ratings = getRatingsList();
+		
+		WebClient.Builder builder = WebClient.builder(); 
 		//For each movie ID, call movie info service and get details
 		List<CatalogItem> catalogs = ratings.stream().map(rating ->{
-			MovieInfo movieInfo = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), MovieInfo.class);
+			//MovieInfo movieInfo = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), MovieInfo.class);
+			MovieInfo movieInfo = webClientBuilder.build().get()
+														  .uri("http://localhost:8082/movies/"+rating.getMovieId())
+														  .retrieve()
+														  .bodyToMono(MovieInfo.class)
+														  .block();
 			return new CatalogItem(movieInfo.getName(), movieInfo.getDesc(), rating.getRating());
 		}).collect(Collectors.toList());
 		return catalogs;
