235fad41d9f1e823d7bb4d77ffd62447ff8a9806
ankit <soodankit1993@gmail.com>
2019-06-01 02:20:09 +0530

    Adding new endpoint to fetch the ratings of movies based on userId

--- ratings-data-service/src/main/java/com/ratingdata/controller/RatingsDataController.java
+++ ratings-data-service/src/main/java/com/ratingdata/controller/RatingsDataController.java
@@ -1,17 +1,36 @@
 package com.ratingdata.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ratingdata.model.Ratings;
+import com.ratingdata.model.Rating;
+import com.ratingdata.model.UserRating;
 
 @RestController
 @RequestMapping("/ratings")
 public class RatingsDataController {
 
-	@RequestMapping("/{movieId}")
-	public Ratings getRatings(@PathVariable("movieId") String movieId) {
-		return new Ratings(movieId, 4);
+	@GetMapping("/{movieId}")
+	public Rating getRatings(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, 4);
+	}
+	
+	@GetMapping("/users/{userId}")
+	public UserRating getUserRatings(@PathVariable("userId") String userId) {
+		UserRating userRating = new UserRating();
+		userRating.setUserRatings(getRatingsList());
+		return userRating;
+	}
+	
+	private List<Rating> getRatingsList(){
+		List<Rating> ratings = new ArrayList<>();
+		ratings.add(new Rating("100",4));
+		ratings.add(new Rating("101",5));
+		return ratings;		
 	}
 }
